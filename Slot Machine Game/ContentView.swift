c42da147a25f4f1b2d2818baf07e7c89d32dabc6
Omar Adel <omar.adel888@hotmail.com>
2024-07-27 22:34:44 +0300

    Initial Commit

---
+++ Slot Machine Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Slot Machine Game
+//
+//  Created by Omar Adel on 27/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Slot Machine Game/Slot_Machine_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_Machine_GameApp.swift
+//  Slot Machine Game
+//
+//  Created by Omar Adel on 27/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Slot_Machine_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
