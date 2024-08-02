6f0532ec5365ddbd8ee936bdd3837191f90b3f3f
Omar Adel <omar.adel888@hotmail.com>
2024-08-02 03:32:55 +0300

    create enum to hold bet values

---
+++ Slot Machine Game/Model/BetAmount.swift
@@ -0,0 +1,13 @@
+//
+//  BetAmount.swift
+//  Slot Machine Game
+//
+//  Created by Omar Adel on 02/08/2024.
+//
+
+import Foundation
+
+enum BetAmount: Int {
+    case ten = 10
+    case twenty = 20
+}
